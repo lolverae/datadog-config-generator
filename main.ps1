547853db4b16481a9812c2569a78aa8113f87636
lolverae <alberto.olvera.espinosa@gmail.com>
2024-02-14 23:21:39 -0600

    Initial commit

---
+++ main.ps1
@@ -0,0 +1,38 @@
+$AssetID = "123"
+$ServerType = "WebServer"
+$TeamName = "Team1"
+$EnvType = "NonProd"
+$EnvironmentName = "Env1"
+$Region = "Region"
+$Hostname = "Host"
+
+$TargetConfig = (Join-Path $ENV:ProgramData 'Datadog/conf.d/windows_service.d/conf.yaml')
+$Services = [pscustomobject]@{
+    'instances' = @(
+        [ordered]@{
+            'hostname' = $Hostname
+            'hostname_fqdn' = $true
+            'tags'                       = @(
+                "Team:$TeamName"
+                "Application_insight_id:$AssetID"
+                "Type:$ServerType"
+                "environment_type:$EnvType"
+                "env:$EnvironmentName"
+                "Region:$Region"
+            )
+
+            'cmd_port' = 5555
+            'gui_port' = 5552
+            
+            'process_config' = 
+              @{
+              'process_collection' = @{
+                'enabled' = $true
+              }}
+            
+        }
+    )
+}
+
+$Services | ConvertTo-Json -Depth 100 | &'C:\tools\yq.exe' eval - --prettyPrint | Out-File $TargetConfig -Encoding UTF8
+
